21e81c1acff64866a22d607f44f8a894090711ba
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-06-21 14:28:29 +0300

    Выполнено задание с разделением проекта на модули

---
+++ setup.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function () {
+var setup = document.querySelector('.setup');
+var HEROES_COUNT = 4;
+  
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarListElement = document.querySelector('.setup-similar-list');
+
+
+var heroes = [];
+
+
+for (var j = 0; j < HEROES_COUNT; j++) {
+  heroes[j] = {
+    name: window.getRandomName(),
+    coatColor: window.getRandomCoat(),
+    eyesColor: window.getRandomEyes()
+   }
+}
+
+var heroesView = function (hero) {
+  hero = similarWizardTemplate.cloneNode(true);
+  hero.querySelector('.setup-similar-label').textContent = heroes[i].name;
+  hero.querySelector('.wizard-coat').style.fill = heroes[i].coatColor;
+  hero.querySelector('.wizard-eyes').style.fill = heroes[i].eyesColor;
+  return hero;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < heroes.length; i++) {
+  fragment.appendChild(heroesView(heroes[i]));
+}
+similarListElement.appendChild(fragment);
+
+setup.querySelector('.setup-similar').classList.remove('hidden');
+})();
