fdf81ad3deaff5779676d48b1fcff6e7913c3d1e
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-06-23 22:54:13 +0300

    работа с сервером (get и post)

---
+++ js/backend.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  var URL_SAVE = 'https://js.dump.academy/code-and-magick';
+  var URL_LOAD = 'https://js.dump.academy/code-and-magick/data';
+  
+  var loadData = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+     
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+       // console.log(xhr.response);
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+       
+    xhr.open('GET', URL_LOAD);
+    xhr.send();
+    
+    
+  };
+  
+  var saveData = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+      
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+       
+    xhr.open('POST', URL_SAVE);
+    xhr.send(data);
+    
+  };
+  
+  
+  window.backend = {
+    load: loadData,
+    save: saveData
+  }
+    
+})();
